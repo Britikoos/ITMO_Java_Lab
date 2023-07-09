9a06ab60e2d1f155fd43aa6a2c46f35d2ada6330
capbr <cap.britikos@gmail.com>
2023-07-09 21:46:10 +0300

    Popytka 3

--- Lab0/src/main/java/ru/britikos/Banks/Builder.java
+++
@@ -1,9 +0,0 @@
-package ru.britikos.Banks;
-
-abstract class Builder {
-    public abstract void SetName(String Name);
-    public abstract void SetCreditPercents(float Percents);
-    public abstract void SetDebitPercents(float Percents);
-    public abstract void SetDepositePercents(float[] Percents);
-    public abstract Bank GetResult();
-}
